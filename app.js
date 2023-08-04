f5382a21caa1868bb1e20062fbd9a2d73c469e71
unknown <mamanialejandra368@gmail.com>
2023-08-04 10:26:46 -0300

    apps,index,views cambios

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
 app.listen(3000, ()=>{
     console.log('servidor corriendo');
     });
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
