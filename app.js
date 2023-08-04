10c0b48f4ef4d57f894677c05914f22ca2d64d94
unknown <mamanialejandra368@gmail.com>
2023-08-04 09:42:04 -0300

    agrego carpeta

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 app.get('/' , function(req, res){
     res.send('Better pagina');
 });
+
+app.use(express.static('public'));
 app.listen(3000, () => console.log('servidor corriendo'));
 
 
